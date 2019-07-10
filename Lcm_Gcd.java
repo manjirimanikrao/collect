07d51c000252f65299df4dc27cd650bf0ef58231
manjirimanikrao <52481055+manjirimanikrao@users.noreply.github.com>
2019-07-10 12:48:37 +0530

    Add files via upload

--- Lcm_Gcd.java
+++ Lcm_Gcd.java
@@ -1,40 +1,30 @@
 import java.util.Scanner;
 
-public abstract class Lcm_Gcd implements g_l_interface
+public class Lcm_Gcd 
 {
+	interface Gcd{
+		int CalGcd(int m1,int m2);
+	}
+	interface Lcm{
+		int CalLcm(int m1,int m2);
+	}
+	
+	
 public static void main(String args[])
 {
-	
-Scanner sc = new Scanner(System.in);
-int m1 = sc.nextInt();
-int m2= sc.nextInt();
 
+	Gcd d=(n1,n2)->{while(n1!=n2) {if(n1>n2) n1-=n2;
+	else
+		n2-=n1;
+	}
+	return n1;
+	};
+	Lcm l=(n1,n2)->n1*n2/d.CalGcd(n1, n2);
+Scanner sc =new Scanner(System.in);
+int n1=sc.nextInt();
+int n2=sc.nextInt();
+System.out.println("Gcd is"+d.CalGcd(n1, n2) );
+System.out.println("Lcm is"+l.CalLcm(n1, n2));
+}
 
-   g_l_interface gl = (p1,p2) ->{
-	   int gcd=1;
-	int n1=p1;
-	int n2=p2;
-    for(int i = 1; i <= n1 && i <= n2; ++i)
-    {
-        if(n1 % i == 0 && n2 % i == 0)
-            gcd = i;
-    }
-    int lcm = (n1 * n2) / gcd;
-    System.out.printf("The LCM of %d and %d is %d.", n1, n2, lcm);
-    System.out.printf("The GCD of %d and %d is %d.", n1, n2, gcd);
-
-	
-
-
-
-};
-
-gl.gcd_lcm_function(m1,m2);
-
-
-	
-	
-	
-	
-
-}}
+}
